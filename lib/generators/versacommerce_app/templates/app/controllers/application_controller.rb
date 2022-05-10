cab0d34f6d286117172adf4def101b72d3ae4c94
Danil Kozub <kozub@versacommerce.de>
2022-05-10 11:46:24 +0300

    Controllers fixes

--- lib/generators/versacommerce_app/templates/app/controllers/application_controller.rb
+++ lib/generators/versacommerce_app/templates/app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :set_locale
+  before_action :set_locale
 
   private
 
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
     session[:locale] = I18n.default_locale if session[:locale].blank?
     session[:locale] = params[:locale] if params[:locale]
     I18n.locale = session[:locale]
-    Rails.application.routes.default_url_options[:locale]= I18n.locale 
+    Rails.application.routes.default_url_options[:locale]= I18n.locale
   end
 
   # prepare variable for the views
